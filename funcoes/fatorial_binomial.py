9aaa8952b51cb868b78027ccd11064b0abd9bd8f
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2023-10-03 14:41:51 -0400

    testes automatizados

---
+++ funcoes/fatorial_binomial.py
@@ -0,0 +1,25 @@
+def fatorial(n):
+    fat = 1
+    while (n > 1):
+        fat = fat * n
+        n = n -1
+    return fat
+
+def numero_binomial(n, k):
+    return fatorial(n) / (fatorial(k) * fatorial(n-k))
+    
+def testa_fatorial():
+    if fatorial(1) == 1:
+        print("Funciona para 1")
+    else:
+        print("Não funciona para 1")
+        
+    if fatorial(2) == 2:
+        print("Funciona para 2")
+    else:
+        print("Não funciona para 2")
+
+    if fatorial(0) == 1:
+        print("Funciona para 0")
+    else:
+        print("Não funciona para 0")
---
+++ testes_automatizados/test_fatorial.py
@@ -0,0 +1,20 @@
+def fatorial(n):
+    i = fat = 1
+    while i <= n:
+        fat = fat * i
+        i = i + 1
+    return fat
+
+def test_fatorial0():
+    assert fatorial(0) == 1
+
+def test_fatorial1():
+    assert fatorial(1) == 1
+
+def test_fatorial4():
+    assert fatorial(4) == 24
+
+def test_fatorial5():
+    assert fatorial(5) == 120
+
+
